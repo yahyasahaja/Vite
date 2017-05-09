f554980374a00f4563086b732bc1f95b471e6a8c
Yahya Sahaja <yahya.sahaja2@gmail.com>
2017-05-09 18:52:39 +0700

    initial update

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+
+app.set('view-engine', 'ejs');
 
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-
+    res.sendFile(__dirname + '/src/home.html');
 });
 
 app.listen(3000, () => console.log('server runnning at port 3000'));

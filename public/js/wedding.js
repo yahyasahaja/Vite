bb3a3ddea20c36834fe28a1ab32329b69ff62a46
Yahya Sahaja <yahya.sahaja2@gmail.com>
2017-06-10 16:21:19 +0700

    angular in edit vite

--- public/js/wedding.js
+++ public/js/wedding.js
@@ -65,6 +65,27 @@ function setContent() {
     });
 }
 
+(function($, ng) {
+    'use strict';
+
+    var $val = $.fn.val; // save original jQuery function
+
+    // override jQuery function
+    $.fn.val = function (value) {
+        // if getter, just return original
+        if (!arguments.length) return $val.call(this);
+
+        // get result of original function
+        var result = $val.call(this, value);
+
+        // trigger angular input (this[0] is the DOM object)
+        ng.element(this[0]).triggerHandler('input');
+
+        // return the original result
+        return result; 
+    }
+})(window.jQuery, window.angular);
+
 function getCookie(name) {
     var value = "; " + document.cookie;
     var parts = value.split("; " + name + "=");
